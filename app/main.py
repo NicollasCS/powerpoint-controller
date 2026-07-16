ec6a7a3d69789f395332a26d90d0350b5c63c7a7
NicollasCS <nicollascane@gmail.com>
2026-07-16 04:35:38 -0300

    🔥 Estrutura correta

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from app import app
-
-# Importa todas as rotas
-from app import auth, slides
-
-# Se precisar de rotas adicionais, coloque aqui
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
